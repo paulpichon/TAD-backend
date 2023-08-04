d88d1457a1d43266d55b3c200683acbdf19b1d6b
paul pichon <paul10_barca@hotmail.com>
2023-08-03 19:58:49 -0600

    Add: se añaden comentarios

--- middlewares/db-validators.js
+++ middlewares/db-validators.js
@@ -1,15 +1,15 @@
-
+//express validator
 const { validationResult } = require("express-validator")
-
 //funcion para validar los campos
 const validarCampos = ( req, res, next ) => {
     //errores
     const errores = validationResult( req );
     //Si existen errores mostrarlos
     if ( !errores.isEmpty() ) {
+        //mostrar errores
         return res.status( 404 ).json( errores );
     }
-    //pasar al siqguiente middleware/funcion
+    //pasar al siguiente middleware/funcion
     next();
 }
 //exports
