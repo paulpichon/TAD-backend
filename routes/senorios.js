5d834593c7297c36de1374260c0f1ea38da856e2
paul pichon <paul10_barca@hotmail.com>
2023-08-16 15:38:59 -0600

    create: se crearon los middlewares para validar los campos del body

--- routes/senorios.js
+++ routes/senorios.js
@@ -1,7 +1,10 @@
 //Aqui se cuentan sobre los 4 señorios de Tlaxcala
-
 //Router Express
 const { Router } = require('express');
+//express validator
+const { check } = require('express-validator');
+//express-validator
+const { validarCampos } = require('../middlewares/validar-campos');
 // Importar controladores
 const { senoriosGet, 
         senorioGet,
@@ -17,7 +20,26 @@ router.get('/', senoriosGet);
 //GET 
 router.get('/:id', senorioGet);
 //POST
-router.post('/', senoriosPost);
+router.post('/', [  
+    //nombre del señorio
+    check('nombre', 'El campo nombre es obligatorio').trim().notEmpty(),
+    //imagen del señorio
+    check('imagen', 'El campo imagen es obligatorio').trim().notEmpty(),
+    //ubicacion del señorio
+    check('ubicacion', 'El campo ubicacion es obligatorio').trim().notEmpty(),
+    //historia antigua del señorio
+    check('historia_antigua', 'El campo historia_antigua es obligatorio').trim().notEmpty(),
+    //economia y comercio del señorio
+    check('economia_comercio', 'El campo economia_comercio es obligatorio').trim().notEmpty(),
+    //participacion en conflictos del señorio
+    check('participacion_conflictos', 'El campo participacion_conflictos es obligatorio').trim().notEmpty(),
+    //alianza con los españoles de cada señorio
+    check('alianza_españoles', 'El campo alianza_españoles es obligatorio').trim().notEmpty(),
+    //el señorio en la epoca colonial
+    check('epoca_colonial', 'El campo epoca_colonial es obligatorio').trim().notEmpty(),
+    //funcion para validar los campos
+    validarCampos
+], senoriosPost);
 //PUT
 router.put('/:id', senoriosPut);
 //DELETE
