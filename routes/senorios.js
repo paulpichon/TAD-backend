52a30a730af9671c1d7866dffbc83f691cdc2a4d
Paul Pichon F <paul10_barca@hotmail.com>
2023-08-24 19:45:34 -0600

    Create: creacion check para la ruta PUT

--- routes/senorios.js
+++ routes/senorios.js
@@ -43,7 +43,30 @@ router.post('/', [
     validarCampos
 ], senoriosPost);
 //PUT
-router.put('/:id', senoriosPut);
+router.put('/:id', [
+    // Verificar el ID
+    check('id', 'El ID no es valido').isMongoId(),
+    // Verificar que el ID exista en la BD
+    check('id').custom( existeIdSenorio ),
+    //nombre del señorio
+    check('nombre', 'El campo nombre no puede estar vacio').optional().trim().notEmpty(),
+    //imagen del señorio
+    check('imagen', 'El campo imagen no puede estar vacio').optional().trim().notEmpty(),
+    //ubicacion del señorio
+    check('ubicacion', 'El campo ubicacion no puede estar vacio').optional().trim().notEmpty(),
+    //historia antigua del señorio
+    check('historia_antigua', 'El campo historia_antigua no puede estar vacio').optional().trim().notEmpty(),
+    //economia y comercio del señorio
+    check('economia_comercio', 'El campo economia_comercio no puede estar vacio').optional().trim().notEmpty(),
+    //participacion en conflictos del señorio
+    check('participacion_conflictos', 'El campo participacion_conflictos no puede estar vacio').optional().trim().notEmpty(),
+    //alianza con los españoles de cada señorio
+    check('alianza_españoles', 'El campo alianza_españoles no puede estar vacio').optional().trim().notEmpty(),
+    //el señorio en la epoca colonial
+    check('epoca_colonial', 'El campo epoca_colonial no puede estar vacio').optional().trim().notEmpty(),
+    //funcion para validar los campos
+    validarCampos
+], senoriosPut);
 //DELETE
 router.delete('/:id', [
     // Verificar el ID
