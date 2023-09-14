422e9f3caef22e643e06ed6ea09bdb7f80715cb3
paul pichon <paul10_barca@hotmail.com>
2023-09-14 16:35:25 -0600

    Funcion id señorio

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,9 +1,11 @@
 //Schema model de Historia
-const Historia = require("../models/historia")
+const Historia = require("../models/historia");
+//Schema model de Señorio
+const Senorio = require("../models/senorio");
 
 //Funcion para validar que el ID de la Historia exista en la BD
 const validarIdHistoria = async( id = '') => {
-    //buscar ID en la BD
+    //Buscar ID en la BD
     const existeIdHistoria = await Historia.findById(id);
     //si no existe el ID mostramos un mensaje
     if ( !existeIdHistoria ) {
@@ -11,7 +13,18 @@ const validarIdHistoria = async( id = '') => {
         throw new Error(`La historia con el ID: ${ id } no existe en la BD`);
     }
 }
+//Funcion para validar el ID del señorio
+const validarIdSenorio = async( id = '' ) => {
+    //Buscar el ID en la BD
+    const existeIdSenorio = await Senorio.findById( id );
+    //Si no existe el ID mostrar una alerta
+    if ( !existeIdSenorio ) {
+        //Mensaje
+        throw new Error(`El señorio con ID: ${ id } no existe en la BD`);
+    }
+}
 //exports
 module.exports = {
-    validarIdHistoria
+    validarIdHistoria,
+    validarIdSenorio
 }
--- routes/senorios.js
+++ routes/senorios.js
@@ -5,6 +5,8 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 //express-validator
 const { validarCampos } = require('../middlewares/validar-campos');
+//Importar funcion para validar el ID de señorio
+const { validarIdSenorio } = require('../helpers/db-validators');
 // Importar controladores
 const { senoriosGet, 
         senorioGet,
@@ -41,7 +43,30 @@ router.post('/', [
     validarCampos
 ], senoriosPost);
 //PUT
-router.put('/:id', senoriosPut);
+router.put('/:id', [
+    //validar el ID
+    check('id', 'El ID no es valido').isMongoId(),
+    //validar que el ID exista en la BD
+    check('id').custom( validarIdSenorio ),
+    //nombre del señorio
+    check('nombre', 'El campo nombre es obligatorio').optional().trim().notEmpty(),
+    //imagen del señorio
+    check('imagen', 'El campo imagen es obligatorio').optional().trim().notEmpty(),
+    //ubicacion del señorio
+    check('ubicacion', 'El campo ubicacion es obligatorio').optional().trim().notEmpty(),
+    //historia antigua del señorio
+    check('historia_antigua', 'El campo historia_antigua es obligatorio').optional().trim().notEmpty(),
+    //economia y comercio del señorio
+    check('economia_comercio', 'El campo economia_comercio es obligatorio').optional().trim().notEmpty(),
+    //participacion en conflictos del señorio
+    check('participacion_conflictos', 'El campo participacion_conflictos es obligatorio').optional().trim().notEmpty(),
+    //alianza con los españoles de cada señorio
+    check('alianza_españoles', 'El campo alianza_españoles es obligatorio').optional().trim().notEmpty(),
+    //el señorio en la epoca colonial
+    check('epoca_colonial', 'El campo epoca_colonial es obligatorio').optional().trim().notEmpty(),
+    //funcion para validar los campos
+    validarCampos
+], senoriosPut);
 //DELETE
 router.delete('/:id', senoriosDelete);
 //exports
