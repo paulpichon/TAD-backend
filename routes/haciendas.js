4a02e04c0e3346d9540b85e2cc573d7892f5c27d
paul pichon <paul10_barca@hotmail.com>
2023-10-03 22:05:01 -0600

    create: creacion de validaciones de campos

--- routes/haciendas.js
+++ routes/haciendas.js
@@ -8,6 +8,10 @@ const { haciendasGet,
         haciendasPut, 
         haciendasDelete 
 } = require('../controllers/haciendas');
+//express validator
+const { check } = require('express-validator');
+//validar campos
+const { validarCampos } = require('../middlewares/validar-campos');
 //Asignar Router a una variable
 const router = Router();
 
@@ -18,7 +22,25 @@ router.get('/', haciendasGet);
 //Obtener una Hacienda por ID
 router.get('/:id', haciendaGet);
 //POST
-router.post('/', haciendasPost);
+router.post('/', [
+    //validar que nombre_hacienda no este vacio
+    check('nombre_hacienda', 'nombre_hacienda es obligatorio').trim().notEmpty(),
+    //validar historia
+    check('historia', 'historia es obligatorio').trim().notEmpty(),
+    //validar ubicacion
+    check('ubicacion', 'ubicacion es obligatorio').trim().notEmpty(),
+    //validar atractivos
+    check('atractivos', 'atractivos es obligatorio').trim().notEmpty(),
+    //validar img
+    check('img', 'img es obligatorio').trim().notEmpty(),
+    //validar alojamiento
+    check('alojamiento', 'alojamiento es obligatorio : el tipo debe ser booleano true/false').isBoolean( {strict : true} ),
+    //validar eventos
+    check('eventos', 'eventos es obligatorio : el tipo debe ser booleano true/false').isBoolean( {strict : true} ),
+    //validar restaurante
+    check('restaurante', 'restaurante es obligatorio : el tipo debe ser booleano true/false').isBoolean( {strict : true} ),
+    validarCampos
+], haciendasPost);
 //PUT
 router.put('/:id', haciendasPut);
 //DELETE
