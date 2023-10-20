d7b6e1b112f7917688d84551c17c0887ae9bf89d
paul pichon <paul10_barca@hotmail.com>
2023-10-19 21:02:19 -0600

    Create: creacion del modelo de Cervezas

---
+++ models/cerveza.js
@@ -0,0 +1,39 @@
+//Modelo de cerveza
+const { Schema, model } = require('mongoose');
+
+//Modelo Cerveza
+const CervezaSchema = Schema({
+    marca_cerveza: {
+        type: String,
+        required: [true, 'La marca de cerveza es obligatoria']
+    },
+    procedencia: {
+        type: String,
+        required: [true, 'La procedencia de la cerveza es obligatoria']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio']
+    },
+    telefono: {
+        type: String,
+        required: [true, 'El telefono es obligatorio']
+    },
+    razon_social: {
+        type: String,
+        required: [true, 'La razon social es obligatoria']
+    },
+    //disponibilidad de la cerveza
+    disponible: {
+        type: Boolean,
+        default: true
+    },
+    img: {
+        type: String,
+        default: 'No imagen'
+    }
+});
+//exports
+module.exports = {
+    CervezaSchema
+}
